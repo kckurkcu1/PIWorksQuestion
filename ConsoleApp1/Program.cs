e257cb59249eaa6c0b2e3478445b2c2abd4257ec
kerem <kerem@MYPC>
2023-10-21 16:27:30 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+
+            string reversed = ReverseWordsInString(input);
+
+            Console.WriteLine("Output: " + reversed);
+            Console.ReadLine();
+        }
+
+        static string ReverseWordsInString(string input)
+        {
+            string[] words = input.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+    }
+}
